7f20ee97b068f808e7da590d4783b87c09917260
Nuno Bento <nbento.neves@gmail.com>
2016-06-25 08:07:17 +0100

    Try to fix problem when get the files to execute into travis cl.

--- scripts/before-deploy.sh
+++ scripts/before-deploy.sh
@@ -3,7 +3,7 @@ echo "Checking the current branch..."
 if [ "$TRAVIS_BRANCH" = 'master' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
 	echo "The current branch is: master"
 	echo "Run the openssl to decrypt the key..."
-	openssl aes-256-cbc -K $encrypted_bde7063f2b13_key -iv $encrypted_bde7063f2b13_iv -in codesigning.asc.enc -out codesigning.asc -d
+	openssl aes-256-cbc -K $encrypted_bde7063f2b13_key -iv $encrypted_bde7063f2b13_iv -in scripts/codesigning.asc.enc -out scripts/codesigning.asc -d
 	gpg --fast-import signingkey.asc
 	echo "Finish run the openssl command"
 fi
--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 echo "Checking the current branch..."
 if [ "$TRAVIS_BRANCH" = 'master' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
-	echo "The current branch is: master"
-	echo "Run maven deploy parameter using sign and build-extras profiles..."
-    mvn deploy -P sign,build-extras --settings ../setting-maven.xml
+    echo "The current branch is: master"
+    echo "Run maven deploy parameter using sign and build-extras profiles..."
+    mvn deploy -P sign,build-extras --settings setting-maven.xml
 fi
