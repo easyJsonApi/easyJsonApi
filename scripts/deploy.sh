1a6f73bd226c68ea061fb2e413a404257d8720a0
Nuno Bento <nbento.neves@gmail.com>
2016-09-30 01:26:10 +0100

    Fix travis deploy

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,5 +3,5 @@ echo "Checking the current branch..."
 if [ "$TRAVIS_BRANCH" = 'master' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
     echo "The current branch is: master"
     echo "Run maven deploy parameter using sign and build-extras profiles..."
-    mvn deploy -P sign,build-extras --settings setting-maven.xml
+    mvn deploy -P sign,build-extras --settings setting-maven.xml -DskipTests=true
 fi
